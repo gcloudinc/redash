870bb8a5bf56791e1a72b2a4e3976e27c72e6b36
Yuki SHIDA <shida@be-agile.jp>
2018-07-19 02:22:31 +0900

    コンテナ起動時にreportのSQLダンプをロードできるようにする

--- docker/root/start.sh
+++ docker/root/start.sh
@@ -27,6 +27,11 @@ export REDASH_ALLOW_SCRIPTS_IN_USER_INPUT="true"
 export REDASH_ALLOW_PARAMETERS_IN_EMBEDS="true"
 EOF
 
+if test -e /tmp/report.sql; then
+  export PGPASSWORD="${DB_PASSWD}"
+  cat /tmp/report.sql | psql -h ${DB_HOST} -U ${DB_USER} -p ${DB_PORT} ${DB_NAME}
+fi
+
 service redis-server start
 service supervisor start
 nginx
